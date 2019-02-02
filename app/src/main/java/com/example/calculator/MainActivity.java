4f1177156e723bb75e4e7a214f4375b1db36ce88
Kevin Hopps <khopps@adobe.com>
2019-02-02 16:36:36 -0600

    Add a button.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Button mEnter;
+    private Keypad mKeypad = new Keypad();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEnter = findViewById(R.id.enter);
+        mEnter.setOnClickListener(mKeypad);
     }
 }
